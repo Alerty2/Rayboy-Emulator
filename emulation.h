af72c5394833d79297d755fffcaedef4dc8f8bb2
Alerty Alerty <alerty264@gmail.com>
2025-07-02 14:46:28 +0200

    Adds instructions until 0x1F

--- emulation.h
+++ emulation.h
@@ -177,44 +177,54 @@ void emulate_cycle(uint8_t* memory, CPU* cpu) {
             cpu->cycles += 8;
             break;
         }
-        case 0x0C:{ // INC C. Increment the value in register r8 by 1.
-            uint8_t result = cpu->bc.C += 1;
+        case 0x0C: { // INC C. Increment the value in register r8 by 1.
+            uint8_t before = cpu->bc.C;
+            uint8_t result = before + 1;
+            cpu->bc.C = result;
+
             // Zero flag
-            if (result == 0){
+            if (result == 0)
                 set_flag(&cpu->af.F, FLAG_Z);
-            }
             else
                 unset_flag(&cpu->af.F, FLAG_Z);
-            // Clear Substract Flag
+
+            // Clear Subtract flag
             unset_flag(&cpu->af.F, FLAG_N);
-            // Half Carry
-            if ((cpu->bc.C & 0x0F) + 1 > 0x0F)
+
+            // Half Carry: Set if there is a carry from bit 3 to bit 4
+            if ((before & 0x0F) + 1 > 0x0F)
                 set_flag(&cpu->af.F, FLAG_H);
             else
                 unset_flag(&cpu->af.F, FLAG_H);
-            cpu->bc.C = result;
+
             cpu->cycles += 4;
             break;
         }
-        case 0x0D:{ // DEC C. Decrement the value in register r8 by 1.
-            uint8_t result = cpu->bc.C -= 1;
+
+        case 0x0D: { // DEC C. Decrement the value in register r8 by 1.
+            uint8_t before = cpu->bc.C;
+            uint8_t result = before - 1;
+            cpu->bc.C = result;
+
             // Zero flag
-            if (result == 0){
+            if (result == 0)
                 set_flag(&cpu->af.F, FLAG_Z);
-            }
             else
                 unset_flag(&cpu->af.F, FLAG_Z);
-            // Enable Substract Flag
+
+            // Set Subtract flag (because it's a subtraction)
             set_flag(&cpu->af.F, FLAG_N);
-            // Half Carry
-            if ((cpu->bc.C & 0x0F) == 0x00)
+
+            // Half Carry: set if borrow from bit 4
+            if ((before & 0x0F) == 0x00)
                 set_flag(&cpu->af.F, FLAG_H);
             else
                 unset_flag(&cpu->af.F, FLAG_H);
-            cpu->bc.C = result;
+
             cpu->cycles += 4;
             break;
         }
+
         case 0x0E:{ // LD C, n8. Copy the value n8 into register C.
             cpu->bc.C = memory[cpu->pc++];
             cpu->cycles += 8;
@@ -260,24 +270,181 @@ void emulate_cycle(uint8_t* memory, CPU* cpu) {
             cpu->cycles += 8;
             break;
         }
-        case 0x14:{// INC D. Increment D by 1.
+        case 0x14: { // INC D. Increments D by 1.
             uint8_t result = cpu->de.D + 1;
-            // Zero Flag
-            if (result == 0){
+            cpu->de.D = result;
+
+            // Zero flag
+            if (result == 0)
                 set_flag(&cpu->af.F, FLAG_Z);
-            }else{
+            else
                 unset_flag(&cpu->af.F, FLAG_Z);
-            }
-            // Substraction Flag
+
+            // Subtraction flag
             unset_flag(&cpu->af.F, FLAG_N);
-            // Half carry flag
-            if ((cpu->bc.C & 0x0F) + 1 > 0x0F)
+
+            // Half Carry flag
+            if ((cpu->de.D & 0x0F) == 0x00)
                 set_flag(&cpu->af.F, FLAG_H);
             else
                 unset_flag(&cpu->af.F, FLAG_H);
+
             cpu->cycles += 4;
             break;
         }
+        case 0x15: { // DEC D. Decrements D by 1.
+            uint8_t result = cpu->de.D - 1;
+
+            cpu->de.D = result;
+
+            // Zero flag
+            if (result == 0)
+                set_flag(&cpu->af.F, FLAG_Z);
+            else
+                unset_flag(&cpu->af.F, FLAG_Z);
+
+            // Subtraction flag
+            set_flag(&cpu->af.F, FLAG_N);
+
+            // Half Carry flag
+            if ((cpu->de.D & 0x0F) == 0x0F)
+                set_flag(&cpu->af.F, FLAG_H);
+            else
+                unset_flag(&cpu->af.F, FLAG_H);
+
+            cpu->cycles += 4;
+            break;
+        }
+        case 0x16: { // LD D, n8. Loads n8 into D register.
+            cpu->de.D = memory[cpu->pc++];
+            cpu->cycles += 8;
+            break;
+        }
+        case 0x17: { // RLA. Moves to the left and saves bit 7 in carry
+            uint8_t old_bit7 = (cpu->af.A & 0x80) >> 7; // Save bit 7
+            uint8_t old_carry = (cpu->af.F & (1 << 4)) ? 1 : 0; // Extracts old carry from Carry Flag
+            cpu->af.A = (cpu->af.A << 1) | old_carry;    // Move to the left and insert bit 7 in bit 0.
+
+            // Update flags
+            unset_flag(&cpu->af.F, FLAG_Z); // Z = 0
+            unset_flag(&cpu->af.F, FLAG_N); // N = 0
+            unset_flag(&cpu->af.F, FLAG_H); // H = 0
+
+            if (old_bit7)
+                set_flag(&cpu->af.F, FLAG_C); // C = 1 if bit 7 = 1
+            else
+                unset_flag(&cpu->af.F, FLAG_C); // C = 0 if not
+
+            cpu->cycles += 4;
+            break;
+        }
+        case 0x18: { // JR e8. Relative Jump to address n16 (encoded as a signed 8 bit)
+            cpu->pc = cpu-> pc += (int8_t)memory[cpu->pc++];
+            cpu->cycles += 12;
+            break;
+        }
+        case 0x19: { // ADD HL, DE . Sum DE to HL
+            uint32_t result = cpu->hl.HL + cpu->de.DE;
+
+            // Half carry
+            if (((cpu->hl.HL & 0x0FFF) + (cpu->de.DE & 0x0FFF)) > 0x0FFF)
+                set_flag(&cpu->af.F, FLAG_H);
+            else
+                unset_flag(&cpu->af.F, FLAG_H);
+
+            // Carry flag
+            if (result > 0xFFFF)
+                set_flag(&cpu->af.F, FLAG_C);
+            else
+                unset_flag(&cpu->af.F, FLAG_C);
+
+            unset_flag(&cpu->af.F, FLAG_N); // Reset N flag
+
+            cpu->hl.HL = (uint16_t)result;
+            cpu->cycles += 8;
+            break;
+        }
+        case 0x1A: { // LD A, [DE]. Copy the byte pointed to by DE into register A.
+            cpu->af.A = memory[cpu->de.DE];
+            cpu->cycles += 8;
+            break;
+        }
+        case 0x1B: { // DEC DE. Decrements DE by 1
+            cpu->de.DE -= 1;
+            cpu->cycles += 8;
+            break;
+        }
+        case 0x1C: { // INC E. Increments E by 1
+            uint8_t result = cpu->de.E + 1;
+
+            // Zero flag
+            if (result == 0)
+                set_flag(&cpu->af.F, FLAG_Z);
+            else
+                unset_flag(&cpu->af.F, FLAG_Z);
+
+            // Clear Subtract flag (N)
+            unset_flag(&cpu->af.F, FLAG_N);
+
+            // Half Carry flag: set if carry from bit 3 to bit 4
+            if ((cpu->de.E & 0x0F) + 1 > 0x0F)
+                set_flag(&cpu->af.F, FLAG_H);
+            else
+                unset_flag(&cpu->af.F, FLAG_H);
+
+            cpu->de.E = result;
+            cpu->cycles += 4;
+            break;
+        }
+        case 0x1D: { // DEC E. Decrements E by 1
+            uint8_t before = cpu->de.E;
+            uint8_t result = before - 1;
+            cpu->de.E = result;
+
+            // Zero flag
+            if (result == 0)
+                set_flag(&cpu->af.F, FLAG_Z);
+            else
+                unset_flag(&cpu->af.F, FLAG_Z);
+
+            // Set Subtract flag (because it's a subtraction)
+            set_flag(&cpu->af.F, FLAG_N);
+
+            // Half Carry: set if borrow from bit 4
+            if ((before & 0x0F) == 0x00)
+                set_flag(&cpu->af.F, FLAG_H);
+            else
+                unset_flag(&cpu->af.F, FLAG_H);
+
+            cpu->cycles += 4;
+            break;
+        }
+        case 0x1E: { // LD E, n8. Copies n8 to E
+            cpu->de.E = memory[cpu->pc++];
+            cpu->cycles += 8;
+            break;
+        }
+        case 0x1F: { // RRA. Rotate A right through carry
+            uint8_t old_bit0 = cpu->af.A & 0x01;
+            uint8_t carry = (cpu->af.F & FLAG_C) ? 1 : 0;
+
+            cpu->af.A = (cpu->af.A >> 1) | (carry << 7);
+
+            // Flags
+            unset_flag(&cpu->af.F, FLAG_Z); // Z = 0
+            unset_flag(&cpu->af.F, FLAG_N); // N = 0
+            unset_flag(&cpu->af.F, FLAG_H); // H = 0
+
+            if (old_bit0)
+                set_flag(&cpu->af.F, FLAG_C);
+            else
+                unset_flag(&cpu->af.F, FLAG_C);
+
+            cpu->cycles += 4;
+            break;
+        }
+
+
 
         case 0x3E: // LD A, d8
             cpu->af.A = memory[cpu->pc++];
